9bb10e07ac430a2c0536f92d70754102ed3d105e
sp1r1tcruh3r <seventiy@bk.ru>
2019-07-04 17:09:04 +0300

    -shock content added

--- buildHTML.js
+++ buildHTML.js
@@ -89,3 +89,48 @@ const solution = (arr) => {
 };
 export default solution;
 // END
+
+/* SHOCK CONTENT LOL
+const propertyActions = [
+  {
+    name: 'body',
+    check: arg => typeof arg === 'string',
+  },
+  {
+    name: 'children',
+    check: arg => arg instanceof Array,
+  },
+  {
+    name: 'attributes',
+    check: arg => arg instanceof Object,
+  },
+];
+
+const getPropertyAction = arg => propertyActions.find(({ check }) => check(arg));
+
+const buildAttrString = attrs =>
+  Object.keys(attrs).map(key => ` ${key}="${attrs[key]}"`).join('');
+
+const buildHtml = (data) => {
+  const [first, ...rest] = data;
+  const root = {
+    name: first,
+    attributes: {},
+    body: '',
+    children: [],
+  };
+  const tag = rest
+    .reduce((acc, arg) => {
+      const { name } = getPropertyAction(arg);
+      return { ...acc, [name]: arg };
+    }, root);
+
+  return [`<${tag.name}${buildAttrString(tag.attributes)}>`,
+    `${tag.body}${tag.children.map(buildHtml).join('')}`,
+    `</${tag.name}>`,
+  ].join('');
+};
+
+export default buildHtml;
+// END
+*/
